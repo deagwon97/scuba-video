922155942cff825d67c088ac4b1591dba43d409c
deagwon97 <curiosity806@naver.com>
2023-10-22 10:51:36 +0000

    add development application

--- deploy/prod/build-push-deploy.sh
+++ deploy/prod/build-push-deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-cd ..
+cd ../../
 
 docker build -t harbor.deagwon.com/scuba-video/scuba-video:latest .
 
 docker push harbor.deagwon.com/scuba-video/scuba-video:latest
 
-cd ./deploy
+cd ./deploy/prod
 
 kubectl apply -k .
 
